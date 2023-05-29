89af28fd0439fb1640378c6ab71cda9d9d20f7c0
dinhdkhoa <kddinh212@gmail.com>
2023-05-29 19:52:42 -0400

    UI and update Input components - UI for HomePage, Product, AsideFilter, SortProductList

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,37 +1,37 @@
-import type { UseFormRegister } from 'react-hook-form'
+import { InputHTMLAttributes } from 'react'
+import type { UseFormRegister, RegisterOptions } from 'react-hook-form'
 
-interface InputProps {
-  type: React.HTMLInputTypeAttribute
-  className?: string
-  placeholder: string
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   errorMessage?: string
-  register: UseFormRegister<any>
-  // rules: RegisterOptions
-  autoComplete?: string
-  name: string
+  clasNameInput?: string
+  classNameError?: string
+  register?: UseFormRegister<any>
+  rules?: RegisterOptions
 }
 
 export default function Input({
   placeholder,
   register,
-  // rules,
+  rules,
   type,
   className,
-
+  classNameError = 'mt-1 min-h-[1.25rem] text-sm text-red-600',
+  clasNameInput = 'w-full rounded-sm border border-gray-300 p-3 outline-none focus:border-gray-500 focus:shadow-sm',
   errorMessage,
   autoComplete,
   name
 }: InputProps) {
+  const registerResult = register && name ? register(name, rules) : {} // đảm bảo ts
   return (
     <div className={className}>
       <input
         type={type}
-        className='w-full rounded-sm border border-gray-300 p-3 outline-none focus:border-gray-500 focus:shadow-sm'
+        className={clasNameInput}
         placeholder={placeholder}
-        {...register(name)}
+        {...registerResult} //xử lí react-hook-form
         autoComplete={autoComplete}
       />
-      <div className='mt-1 min-h-[1.25rem] text-sm text-red-600'>{errorMessage}</div>
+      <div className={classNameError}>{errorMessage}</div>
     </div>
   )
 }
